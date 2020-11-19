62f411a9a5cdb94e1b396f22cdb059c6d28e661b
Bill Mårtensson <bill@magictechnology.se>
2020-11-19 20:53:01 +0100

    Initial Commit

---
+++ WidgetIntro/WidgetIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetIntroApp.swift
+//  WidgetIntro
+//
+//  Created by Bill Martensson on 2020-11-19.
+//
+
+import SwiftUI
+
+@main
+struct WidgetIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
